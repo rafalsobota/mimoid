e6682e32e91cc8bdcd5327726af3f2eef92d2b8b
Rafał Sobota <ravsobota@gmail.com>
2022-03-15 19:51:14 +0100

    Foix

--- client/src/assets/paths.ts
+++ client/src/assets/paths.ts
@@ -5,5 +5,5 @@
 
 // export const walkingAlienGLTFPath = getDownloadURL(ref(storage, 'assets/Walking/Alien.gltf'));
 // export const walkingAlienGLTFPath = 'https://firebasestorage.googleapis.com/v0/b/mimoid.appspot.com/o/Alien.gltf?alt=media&token=e8391ba9-64ba-4e82-80df-43a7a3f19b3a'
-export const walkingAlienGLTFPath = 'https://firebasestorage.googleapis.com/v0/b/mimoid.appspot.com/o/Alien.gltf?alt=media'
-// export const walkingAlienGLTFPath = '/assets/Walking/Alien.gltf';
+// export const walkingAlienGLTFPath = 'https://firebasestorage.googleapis.com/v0/b/mimoid.appspot.com/o/Alien.gltf?alt=media'
+export const walkingAlienGLTFPath = '/assets/Walking/Alien.gltf';
